d1638cdb6db3bf4c87050bc56ca70c5b60eb0a20
Hary Selman <hary.selman@gmail.com>
2026-07-15 02:47:08 +0800

    refactor(frontend): remove obsolete deployment modal

--- packages/frontend-host/src/features/starbase/components/project-detail/index.ts
+++ packages/frontend-host/src/features/starbase/components/project-detail/index.ts
@@ -1,6 +1,5 @@
 // Project Detail extracted components and utilities
 export * from './project-detail-utils'
-export { EngineDeploymentsModal } from './EngineDeploymentsModal'
 export { EngineAccessModal } from './EngineAccessModal'
 export { ProjectDeploymentTargetsModal } from './ProjectDeploymentTargetsModal'
 export { FolderLoader, CurrentPath, TreePicker } from './FolderTreeHelpers'
